e6e123e90c0cc5419d2efea09c196c1830a0e457
andersonkaiti <anderkaiti@gmail.com>
2026-03-15 12:18:39 -0300

    :bug: fix(env): fix API URL typo and pass env variables individually

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { env } from '@config/env'
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_APP_URL,
+  baseURL: env.NEXT_PUBLIC_API_URL,
 })
 
 authClient.signIn.social({
